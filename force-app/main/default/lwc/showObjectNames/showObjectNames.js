5d7d645a7e8beb49705a62c09d6d882e6945ef29
Vineet Goel <vineet.goel@kaplan.edu>
2020-07-16 18:50:33 +0530

    Platform Cache Demo

---
+++ force-app/main/default/lwc/showObjectNames/showObjectNames.js
@@ -0,0 +1,23 @@
+import { LightningElement, track, wire } from 'lwc';
+//import getObjectNamesFromCache from '@salesforce/apex/ObjectNameController.objectNamesFromCache';
+//import getObjectNamesbyCacheBuilderInterface from '@salesforce/apex/ObjectNameController.objectNamesbyCacheBuilderInterface';
+import getObjectNamesbySOQL from '@salesforce/apex/ObjectNameController.objectNamesbySOQL';
+
+export default class ShowObjectNames extends LightningElement {
+    @track objectNames;
+    selectedObj;
+    @wire(getObjectNamesbySOQL) wiredObjectNames({error, data}){
+        if(data){
+            this.objectNames = data;
+            this.error = undefined;
+        } else if(error){
+            this.error= error;
+            this.objectNames = undefined;
+        }
+    }
+
+    handleonChange(event){
+        this.selectedObj = event.target.value;
+        console.log(this.selectedObj);
+    }
+}
